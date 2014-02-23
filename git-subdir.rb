0363cd353b7529ad7f2b3060fa32f8fb2956143e
Faraaz Khan <faraaz@rationalizeit.us>
2014-02-23 14:04:27 -0500

    adds gitsubdir head

---
+++ git-subdir.rb
@@ -0,0 +1,17 @@
+require "formula"
+
+class GitSubdir < Formula
+  homepage "https://github.com/andreyvit/git-subdir"
+  url "git@github.com:andreyvit/git-subdir.git", :using => :git
+  sha1 "ac4128b74e8b6a6916d634e7313ee25d80afd579"
+
+  depends_on 'git'
+  def install
+    system "make", "install" 
+  end
+
+  test do
+    system "git init"
+    system "git subdir"
+  end
+end
